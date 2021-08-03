603c5c988d1705da6c053b4fc98f62a279916479
Brian Holt <bholtbholt@icloud.com>
2021-08-02 18:17:28 -0700

    Add redeemed boolean to Incentive

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,2 +1,4 @@
 class Incentive < ApplicationRecord
+  scope :redeemed, ->  { where(redeemed: true ) }
+  scope :available, -> { where(redeemed: false) }
 end
--- test/models/incentive_test.rb
+++ test/models/incentive_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class IncentiveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @available_incentive = create(:incentive)
+    @redeemed_incentive = create(:incentive, redeemed: true)
+  end
+
+  test 'should return available incentives' do
+    assert_equal [@available_incentive], Incentive.available
+  end
+
+  test 'should return redeemed incentives' do
+    assert_equal [@redeemed_incentive], Incentive.redeemed
+  end
 end

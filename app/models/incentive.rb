c4ed0a8e93304d274427f87cd23663f7c40b6852
Brian Holt <bholtbholt@icloud.com>
2021-08-03 21:14:59 -0700

    Add NullIncentive to capture invalid incentives

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,4 +1,21 @@
 class Incentive < ApplicationRecord
   scope :redeemed, ->  { where(redeemed: true ) }
   scope :available, -> { where(redeemed: false) }
+
+  class NullIncentive
+    attr_reader :errors
+
+    # NullIncentive will always have an error present so it functions
+    # more like an actual Incentive with a problem (that it's nil)
+    def initialize
+      @errors = ActiveModel::Errors.new(self)
+      @errors.add(:base, "No incentives are available")
+    end
+
+    def update(_ = nil)
+      false
+    end
+  end
+
+  Null = NullIncentive.new.freeze
 end
--- test/models/incentive_test.rb
+++ test/models/incentive_test.rb
@@ -13,4 +13,15 @@ class IncentiveTest < ActiveSupport::TestCase
   test 'should return redeemed incentives' do
     assert_equal [@redeemed_incentive], Incentive.redeemed
   end
+
+  describe '.NullIncentive' do
+    test '#update should return false' do
+      refute Incentive::Null.update(anything: false)
+    end
+
+    test '#errors should return an ActiveModel::Errors object' do
+      assert_kind_of ActiveModel::Errors, Incentive::Null.errors
+      assert_equal "No incentives are available", Incentive::Null.errors.full_messages.to_sentence
+    end
+  end
 end

4285ef6b9c6726c136407e31ea5445d019fb270a
Brian Holt <bholtbholt@icloud.com>
2021-08-04 22:06:52 -0700

    Run Prettier on endpoints

--- app/javascript/api/endpoints.ts
+++ app/javascript/api/endpoints.ts
@@ -6,10 +6,13 @@ export const getIncentives = async (): Promise<Incentive[]> => {
   return null;
 };
 
-export const updateIncentive = async (id: number, params: Partial<Incentive>): Promise<Incentive> => {
+export const updateIncentive = async (
+  id: number,
+  params: Partial<Incentive>,
+): Promise<Incentive> => {
   const resp = await fetch(`/api/incentives/${id}`, {
     method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(params),
   });
   if (resp.ok) {
